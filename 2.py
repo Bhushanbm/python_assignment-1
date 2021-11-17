2be3dd9215beeb79a6ff08aff70d4c83acd9b1dc
Bhushanbm <bhushanmere706@gmail.com>
2021-11-17 21:54:31 +0530

    commit

---
+++ 2.py
@@ -0,0 +1,6 @@
+a = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+b = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+c = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(*a, sep = "\n");
+print(*b, sep = ", ");
+print(*c, sep = "\n");
