c52e49338d166ad91c81b60202b1189581a5b589
Bhushanbm <bhushanmere706@gmail.com>
2021-11-17 21:55:00 +0530

    commit

---
+++ 4.py
@@ -0,0 +1,10 @@
+age = int(input("Hi, What is your age :"))
+print(age)
+weight = float(input("what is your weight in kg:"))
+print(weight)
+height = float(input("what is your height in metres:"))
+print(height)
+BMI = round(weight/ (height * height), 1)
+print(f"your BMI is {BMI}");
+
+
