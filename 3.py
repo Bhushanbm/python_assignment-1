b3c8b9cf07fa72f238f324d6327bd680cc27c1e7
Bhushanbm <bhushanmere706@gmail.com>
2021-11-17 21:54:45 +0530

    commit

---
+++ 3.py
@@ -0,0 +1,8 @@
+b = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+weekDays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+print(str(b).strip(""));
+print(weekDays)
+print(1<2)
+print(99 >= 8)
+print(345678 + 77777 < 66666 - 90)
+print(7896 * 5555 == 8989898)
